ad72720865aeeb5791111fc2911f136740ab3279
Ayoub Essabiry <aessabir@e2r6p2.1337.ma>
2023-04-28 16:40:09 +0100

    mariadb done

--- src/requirements/mariadb/tools/Script.sh
+++ src/requirements/mariadb/tools/Script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/etc/init.d/mysql restart
-echo "GRANT ALL ON *.* TO 'aessabir'@'localhost' IDENTIFIED BY 'password123' WITH GRANT OPTION;" | mysql
+/etc/init.d/mysql restart 
+echo "GRANT ALL ON *.* TO 'aessabir'@'%' IDENTIFIED BY 'password123' WITH GRANT OPTION;" | mysql
 echo "FLUSH PRIVILEGES;" | mysql
