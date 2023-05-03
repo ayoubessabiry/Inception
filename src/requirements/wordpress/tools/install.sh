a1e3b063020ded7b0a328c80dd61e08387541f88
Ayoub Essabiry <aessabir@e2r6p2.1337.ma>
2023-05-03 12:59:21 +0100

    wordpress done

---
+++ src/requirements/wordpress/tools/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wp --allow-root core download
+
+wp --allow-root config create --dbname=MYdb --dbuser=aessabir --dbpass=password123 --dbhost=db
+
+wp --allow-root db create
+
+
+wp --allow-root core install --url=localhost --title=test --admin_user=aessabir --admin_password=password123 --admin_email=aessabir@student.1337.ma
+
+php-fpm7.3 -RF
